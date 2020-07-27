8e84ca50981ab43cbace2fade5eb821607e5415b
2670211238@qq.com <2670211238@qq.com>
2020-07-27 10:12:47 +0800

    Initial commit

---
+++ yaoyongkang_1905c/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for yaoyongkang_1905c project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yaoyongkang_1905c.settings')
+
+application = get_asgi_application()
